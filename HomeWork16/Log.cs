5753ee27addef141df323687264ffa08c105bccd
Parenb-iz-MInska <vladike86@gmail.com>
2022-09-04 22:15:34 +0300

    fix solution reference

---
+++ HomeWork16/Log.cs
@@ -0,0 +1,11 @@
+﻿namespace HomeWork16
+{
+    public class Log
+    {
+        public static List<string> LogInfo { get; set; } = new();
+        public static void LogEntry(string message)
+        {
+            LogInfo.Add(message);
+        }
+    }
+}
---
+++ HomeWork16/Program.cs
@@ -0,0 +1,32 @@
+﻿using HomeWork16;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+Inventory inventory = new();
+
+/*using (FileStream fs = new FileStream("inventory.json", FileMode.OpenOrCreate))
+{
+    Console.WriteLine("Download from JSON");
+    inventory = await JsonSerializer.DeserializeAsync<Inventory>(fs);
+}
+Console.WriteLine("-=-=-=-=-=-=-=-=-");
+foreach (var item in inventory.Products)
+{
+    Console.WriteLine(item);
+}*/
+/*inventory.Products.Add(new ProductClass(1, 1, "hello"));
+inventory.Products.Add(new ProductClass(2, 3, "hello"));
+inventory.Products.Add(new ProductClass(4, 6, "hello"));*/
+BinaryFormatter formatter = new();
+
+Console.WriteLine("Download from binary file");
+using (FileStream fs = new("people.dat", FileMode.OpenOrCreate))
+{
+    inventory = (Inventory)formatter.Deserialize(fs);
+}
+foreach (var item in inventory.Products)
+{
+    Console.WriteLine(item);
+}
