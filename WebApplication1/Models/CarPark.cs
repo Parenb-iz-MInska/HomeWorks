3eb5f06df87111690374ca0e5d08db0897b83292
Parenb-iz-MInska <vladike86@gmail.com>
2022-09-04 13:19:27 +0300

    Done HomeWork 18-19

---
+++ WebApplication1/Models/CarPark.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication1.Models
+{
+    public class CarPark
+    {
+        public List<Car> Cars { get; set; } = new();
+        public CarPark()
+        {
+
+        }
+        public void AddCar(Car car)
+        {
+            Cars.Add(car);
+        }
+        public void RemoveCar(int targetCarId)
+        {
+            Cars.Remove(Cars.Find(car => targetCarId == car.Id));
+        }
+    }
+}
