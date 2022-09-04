fae97729cb8f29aa6475384662a288d72bb0e530
Parenb-iz-MInska <84907271+Parenb-iz-MInska@users.noreply.github.com>
2022-09-04 22:16:50 +0300

    Done HomeWork 18-19 (#3)
    * Done HomeWork 18-19
    
    * Deleted index
    
    * fix solution reference
    
    * Some shit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static CarPark carPark = new();
+        public IActionResult GetCars()
+        {
+            return View(carPark.Cars);
+        }
+        [HttpGet]
+        public IActionResult AddCar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCar(Car car)
+        {
+            carPark.AddCar(car);
+            return RedirectToAction("GetCars", carPark.Cars);
+        }
+        public IActionResult EditCar(int id)
+        {
+            return View(carPark.Cars.Find(car => car.Id == id));
+        }
+        [HttpPost]
+        public IActionResult EditCar(Car car)
+        {
+            var exsistCar = carPark.Cars.Find(x => x.Id == car.Id);
+            exsistCar.Price = car.Price;
+            exsistCar.Name = car.Name;
+            exsistCar.Color = car.Color;
+            return RedirectToAction("GetCars", carPark.Cars);
+        }
+        [HttpPost]
+        public IActionResult RemoveCar(int id)
+        {
+            carPark.RemoveCar(id);
+            return RedirectToAction("GetCars", carPark.Cars);
+        }
+        [HttpGet]
+        public IActionResult RemoveCarView(int id)
+        {
+            return View("RemoveCar",carPark.Cars.Find(car => car.Id == id));
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/Models/Car.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApplication1.Models
+{
+    public class Car
+    {
+        private static int _globalId = 0;
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Price { get; set; }
+        public string Color { get; set; } = string.Empty;
+        public Car()
+        {
+            Id = _globalId;
+            ++_globalId;
+        }
+        public Car(string name, int price, string color)
+        {
+            Id = _globalId;
+            Name = name;
+            Price = price;
+            Color = color;
+            _globalId++;
+        }
+    }
+}
