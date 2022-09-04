2524aba957e626e7acccbc5b7022b802e22a6bdc
Parenb-iz-MInska <vladike86@gmail.com>
2022-09-04 13:31:55 +0300

    Deleted index

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -7,10 +7,6 @@ namespace WebApplication1.Controllers
     public class HomeController : Controller
     {
         public static CarPark carPark = new();
-        public IActionResult Index()
-        {
-            return View();
-        }
         public IActionResult GetCars()
         {
             return View(carPark.Cars);
